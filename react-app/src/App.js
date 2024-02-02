2d6c0878121f8e191a9d46d12ff24bf00ca19b55
Ravi <ravi.kakadia@techholding.co>
2024-02-02 15:12:52 +0530

    Ravi Component added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Ravi } from './Ravi';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Ravi />
     </div>
   );
 }
---
+++ react-app/src/Ravi.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Ravi = () => {
+  return (
+    <div>Ravi</div>
+  )
+}
