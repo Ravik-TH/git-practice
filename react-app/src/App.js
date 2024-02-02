aa0e408347f51c84dc16a37eb3fb91811d649faa
codecolonel07 <kushalmpatel07@gmail.com>
2024-02-02 15:06:26 +0530

    kushal component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Kushal from './Kushal';
 
 function App() {
   return (
     <div className="App">
-      
+      <Kushal/>
     </div>
   );
 }
---
+++ react-app/src/Kushal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Kushal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Kushal
